9ab18166221725aad73ab1ee2ad3af23b1d6a519
sigizmunde <1816579@gmail.com>
2024-04-20 20:01:40 +0300

    Change config to build properly

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   output: {
     filename: 'app.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+    publicPath: '',
     assetModuleFilename: 'images/[name][ext][query]',
   },
   plugins: [
